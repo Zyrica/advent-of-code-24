7395be941657b5ec9f703973b3e14a74d345d498
Zyrica <drevin@zyrica.com>
2024-12-11 09:32:17 +0100

    Day 11

--- day-11.js
+++ day-11.js
@@ -6,16 +6,30 @@ const day = 11;
 console.log(`day-${day}.js`);
 
 const testInput = `125 17`;
-const testAnswer1 = 55312;
-const testAnswer2 = null;
+const testAnswer = 55312;
 
 const input = readFileSync(`input-day-${day}.txt`, "utf-8");
 
 function parse(input) {
-  return input.replaceAll("\r", "").split(" ").map(Number);
+  let stones = {};
+  input
+    .replaceAll("\r", "")
+    .split(" ")
+    .forEach((l) => {
+      add(stones, Number(l));
+    });
+  return stones;
+}
+function add(stones, stone, amount = 1) {
+  if (!stones[stone]) stones[stone] = 0;
+  stones[stone] += amount;
 }
 
+const transformCach = {};
 function transform(stone) {
+  if (transformCach[stone]) {
+    return transformCach[stone];
+  }
   let result;
   const string = stone.toString();
   if (stone === 0) {
@@ -26,37 +40,42 @@ function transform(stone) {
   } else {
     result = [Number(stone) * 2024];
   }
+  transformCach[stone] = result;
   return result;
 }
+
 function blink(stones) {
-  let newStones = [];
-  for (let stone of stones) {
-    newStones.push(...transform(stone));
+  let newStones = {};
+  for (let stone of Object.keys(stones)) {
+    const count = stones[stone];
+    const [a, b] = transform(Number(stone));
+    add(newStones, a, count);
+    if (b || b === 0) add(newStones, b, count);
   }
   return newStones;
+}
+function countStones(stones) {
+  return Object.values(stones).reduce((a, b) => a + b, 0);
 }
 function blinks(stones, blinks) {
   let result = stones;
   for (let i = 0; i < blinks; i++) {
     result = blink(result);
-    console.log(i + 1, result.length);
   }
   return result;
 }
 function question1(input) {
   let stones = parse(input);
-  return blinks(stones, 25).length;
+  return countStones(blinks(stones, 25));
 }
 
-assert.strictEqual(question1(testInput), testAnswer1);
+assert.strictEqual(question1(testInput), testAnswer);
 
 console.log("Answer question 1:", question1(input));
 
 function question2(input) {
   let stones = parse(input);
-  return blinks(stones, 75).length;
+  return countStones(blinks(stones, 75));
 }
-
-assert.strictEqual(question2(testInput), testAnswer2);
 
 console.log("Answer question 2:", question2(input));
